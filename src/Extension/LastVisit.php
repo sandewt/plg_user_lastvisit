1f63b9cc3a12ea7fd9e6107930371ec30c23bb4b
jsanders <j53.sanders@gmail.com>
2023-05-06 12:41:45 +0200

    Update LastVisit.php

--- src/Extension/LastVisit.php
+++ src/Extension/LastVisit.php
@@ -14,6 +14,7 @@ namespace EWT\Plugin\User\LastVisit\Extension;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Database\ParameterType;
 
 defined('_JEXEC') or die;
 
@@ -53,7 +54,24 @@ final class LastVisit extends CMSPlugin
             return;
         }
 
-        $date = HTMLHelper::_('date', $options['lastvisitDate'], $lang->_('DATE_FORMAT_LC2'));
+        $db = $this->getDatabase();
+        $query = $db->getQuery(true)
+            ->select($db->quoteName('log_date'))
+            ->from($db->quoteName('#__action_logs'))
+            ->where($db->quoteName('user_id') . ' = :id')
+            ->where($db->quoteName('message_language_key') . ' = ' . $db->quote('PLG_ACTIONLOG_JOOMLA_USER_LOGGED_IN'))
+            ->order($db->quoteName('log_date') . ' DESC')
+            ->bind(':id', $user->id, ParameterType::INTEGER);
+        $db->setQuery($query);
+
+        try
+        {
+            $result = $db->loadRowList();
+        } catch (RuntimeException $e) {
+            $app->enqueueMessage($lang->_('JERROR_AN_ERROR_HAS_OCCURRED'), 'error');
+        }
+        
+        $date = HTMLHelper::_('date', $result[1][0], $lang->_('DATE_FORMAT_LC2'));
 
         $app->enqueueMessage(sprintf($lang->_('PLG_USER_LASTVISIT_DATE'), $date), 'info');
     }
